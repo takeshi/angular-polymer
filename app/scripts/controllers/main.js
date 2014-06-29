1f63cd3fc6d808bd46149eb2641217cda801d16d
近藤健 <takeshi@kondouken-no-MacBook-Air.local>
2014-06-29 14:54:39 +0900

    update

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,12 +9,8 @@ angular.module('y2App')
     ];
 
     $scope.test = function(value){
-      console.log("test" ,value);
-      console.log("text",$scope.text);
-      console.log("id",document.getElementById("sample").inputValue);
       $scope.text = value;
     }
-    console.log("start");
-
     $scope.text = "sampleaaa";
+    $scope.sampleLabel = "sample Label xxx";
   });
--- app/scripts/services/bindngmodel.js
+++ app/scripts/services/bindngmodel.js
@@ -1,63 +1,75 @@
 'use strict';
 
+
 angular.module('y2App')
   .service('bindngmodel', function bindngmodel($timeout,$parse,$browser) {
     return function(scope,element,attrs){
-
-if(!attrs.ngModel){
-  return;
-}
-
-var ngModelGet = $parse(attrs.ngModel),
-    ngModelSet = ngModelGet.assign;
-
 var input = element[0];
-    
-$timeout(function(){
-  input.inputValue = ngModelGet(scope);
-},500);
 
-var timeout;
-
-var deferListener = function(ev) {
-  if (!timeout) {
-    timeout = $browser.defer(function() {
-      listener(ev);
-      timeout = null;
-    });
-  }
-};
-
-var listener = function(event){
-  ngModelSet(scope, input.inputValue);
-  scope.$apply();
+if(attrs.ngModel){
+  bindNgModel('ngModel','inputValue',true);
 }
 
-input.addEventListener('change',function(event){
-  deferListener(event);
-});
+function toModel(modelGet){
+  var ngModelSet = modelGet.assign;
 
-input.addEventListener('keydown',function(event){
-  var key = event.keyCode;
+  var timeout;
 
-  // ignore
-  //    command            modifiers                   arrows
-  if (key === 91 || (15 < key && key < 19) || (37 <= key && key <= 40)) return;
+  var deferListener = function(ev) {
+    if (!timeout) {
+      timeout = $browser.defer(function() {
+        listener(ev);
+        timeout = null;
+      });
+    }
+  };
 
-  
-  deferListener(event);
-});
-
-var first = true;
-
-scope.$watch(attrs.ngModel,function ngModelWatch() {
-  var value = ngModelGet(scope);
-  if(first){
-    first = false;
-    return;
+  var listener = function(event){
+    ngModelSet(scope, input.inputValue);
+    scope.$apply();
   }
-  input.inputValue = value;
-});
+
+  input.addEventListener('change',function(event){
+    deferListener(event);
+  });
+
+  input.addEventListener('keydown',function(event){
+    var key = event.keyCode;
+
+    // ignore
+    //    command            modifiers                   arrows
+    if (key === 91 || (15 < key && key < 19) || (37 <= key && key <= 40)) return;
+
+    deferListener(event);
+  });
+
+}
+
+function toInput(ngModelGet,attrName,inputName){
+
+  // placeholderが表示されてしまうのを防ぐ
+  $timeout(function(){
+    input[inputName] = ngModelGet(scope);
+  },400);
+
+  var first = true;
+  scope.$watch(attrName,function ngModelWatch() {
+    if(first){
+      first = false;
+      return;
+    }
+    var value = ngModelGet(scope);
+    input[inputName] = value;
+  });
+}
+
+function bindNgModel(attrName,inputName,towWay){
+  var ngModelGet = $parse(attrs[attrName]);
+  toInput(ngModelGet,attrs[attrName],inputName);
+  if(towWay){
+    toModel(ngModelGet,attrs[attrName],inputName);
+  }
+}
 
     };
   });
